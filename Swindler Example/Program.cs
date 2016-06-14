4029d44385b19272d1c8e7b2641bd10838a6fa6c
HoboRobo123 <arobilla@hotmail.com>
2016-06-13 20:35:50 -0400

    Update

--- Swindler Example/Program.cs
+++ Swindler Example/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Swindler_Example
 {
@@ -10,6 +11,19 @@ namespace Swindler_Example
     {
         static void Main(string[] args)
         {
+            StreamWriter sw = new StreamWriter(String.Format(@"{0}\secret_plan.txt", Environment.CurrentDirectory));
+            sw.WriteLine("How I'll defeat Captain Amazing");
+            sw.WriteLine("Another genius secret plan by The Swindler");
+            sw.Write("I'll create an army of clones and ");
+            sw.WriteLine("unleash them upon the citizens of Objectville.");
+            string location = "the mall";
+            for (int number = 0; number <= 6; number++)
+            {
+                sw.WriteLine("Clone #{0} attacks {1}", number, location);
+                if (location == "the mall") { location = "downtown";  }
+                else { location = "the mall"; }
+            }
+            sw.Close();
         }
     }
 }
